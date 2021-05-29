0b37feba4a6168df94d96c478eab1c7a45d0d40b
sterlingking1990 <izundukingsleyemeka@gmail.com>
2021-05-29 11:56:48 +0100

    move test to appropriate package

--- app/src/androidTest/java/com/giftinapp/business/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/giftinapp/business/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.business;
+package com.giftinapp.business;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.business", appContext.getPackageName());
+        assertEquals("com.giftinapp.business", appContext.getPackageName());
     }
 }
